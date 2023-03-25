711016aab6f196af5d3d5fbbb67daa42f9878f30
7td <thedresr@mail.ru>
2023-03-25 21:46:48 +0300

    #5 add image in Gallery (Frontend)

--- learnProjectFrontend/src/pages/Gallery/Gallery.tsx
+++ learnProjectFrontend/src/pages/Gallery/Gallery.tsx
@@ -1,10 +1,16 @@
 import styles from './Gallery.module.scss';
 import Triangle from '../../assets/svg/Triangle.svg';
 import RefreshCircleImage from '../../assets/svg/RefreshCicle.svg';
+import React from "react";
 
 
 export const Gallery = () => {
+    const [selectedImage, setSelectedImage] = React.useState<string>();
 
+    const handleChange = (event: any) => {
+        console.log(event.target.files);
+        setSelectedImage(URL.createObjectURL(event.target.files[0]));
+    }
 
     return (
         <>
@@ -12,14 +18,36 @@ export const Gallery = () => {
                 <h2>Gallery</h2>
             </div>
             <div className={styles.ContentBody}>
+
+
+
                 <div className={styles.ContentBodyTitleImage}>
-                    <div className={styles.Image}></div>
+
+                    {selectedImage ? (
+                        <div className={styles.Image}>
+                            <img src={selectedImage} alt="image" width={380}/>
+                        </div>
+                    ) : (<div className={styles.Image} />)}
+
                     <span>TitleImage</span>
                     <div className={styles.TitleImageBtns}>
-                        <button className={styles.BlueBtn}>Add image</button>
-                        <button className={styles.ReadBtn}>Cancel</button>
+                        <label>
+                            <input type="file" onChange={handleChange} />
+                            <span>Add image</span>
+                        </label>
+                        <button
+                            onClick={() => setSelectedImage('')}
+                            className={styles.ReadBtn}>
+                            Cancel
+                        </button>
+                    </div>
+                    <div className={styles.ContentBodyTitleImagePush}>
+                        <button>Save image</button>
                     </div>
                 </div>
+
+
+
                 <div className={styles.ContentBodyList}>
                     <div className={styles.List}/>
                     <div className={styles.ListSelector}>
